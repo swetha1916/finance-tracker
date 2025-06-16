24f81d34f8588786750752e8f7508f8ea6c53750
swetha1916 <swetha.anantha@gmail.com>
2025-06-16 14:26:53 +0400

    Added password conditions and validation, disabled submit button accordingly

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,8 +4,32 @@ import axios from 'axios';
 function Signup({onSignup}) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [validationMsg, setValidationMsg] = useState("");
+    const [isValid, setIsValid] = useState(false);
     const [error, setError] = useState('');
 
+    // Regular expression for at least 1
+    const hasNumber = /\d/;
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
+
+    const validatePassword = (pwd) => {
+        if (pwd.length < 8) {
+            setValidationMsg("Password is too short.");
+        } else if (!hasNumber.test(pwd)) {
+            setValidationMsg("Password is missing a numerical character.");
+        } else if (!hasSpecialChar.test(pwd)) {
+            setValidationMsg("Password is missing a special character.");
+        } else {
+            setValidationMsg("Password meets all conditions.");
+            setIsValid(true);
+        }   
+    };
+
+    const handlePasswordChange = (e) => {
+        const pwd = e.target.value;
+        setPassword(pwd);
+        validatePassword(pwd);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -34,11 +58,22 @@ function Signup({onSignup}) {
 
                 <div>
                     <label>Password:</label><br/>
-                    <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} maxLength={20} required/>
-                    <small style={{color:'gray'}}>Max 20 characters</small>
+                    <input type="text" placeholder="Password" value={password} onChange={handlePasswordChange} maxLength={20} required/>
+                    <small style={{color:'gray'}}>Max 20 characters</small><br/>
+                    <small style={{ display: 'block', marginTop: 5 }}>
+                        Password rules:
+                        <ul style={{ marginTop: 5, marginBottom: 5 }}>
+                            <li>Must include at least 1 number</li>
+                            <li>Must include at least 1 special character (!@#$%^&*(),.?":{}\|)</li>
+                            <li>Must be at least 8 characters long</li>
+                        </ul>
+                    </small>
+                    <div style={{ color: validationMsg === "Password meets all conditions." ? 'green' : 'red', marginTop: 5 }}>
+                    {validationMsg}
+                    </div>
                 </div>
 
-                <button type="submit">Sign up</button>
+                <button type="submit" disabled={!isValid}>Sign up</button>
             </form>
             {error && <p style={{color:'red'}}>{error}</p>}
         </div>
